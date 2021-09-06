ce12370d3b710187377b69b2e34c61a5df48122b
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2021-09-06 10:48:07 +0200

    Moved code out into methods to clean up

--- src/se/iths/java/patrik/FizzBuzz.java
+++ src/se/iths/java/patrik/FizzBuzz.java
@@ -6,25 +6,33 @@ public class FizzBuzz {
 
         // Alt: 1 IF / Fori
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0)           // (i % 3*5 == 0) eller (i % 15 == 0) Bara delbart med 15
-                System.out.print("FizzBuzz, ");
-            else if (i % 3 == 0)
-                System.out.print("Fizz, ");
-            else if (i % 5 == 0)
-                System.out.print("Buzz, ");
-            else
-                System.out.print(i + ", ");
+            printFizzBuzzForValue(i);
         }
 
         // Alt: 2 SWITCH, mindre än 15 alternativ
         for (int i = 1; i <= 100; i++) {
-            switch (i % 15) {
-                case 0 -> System.out.println("FizzBuzz");
-                case 3, 6, 9, 12 -> System.out.println("Fizz");
-                case 5, 10 -> System.out.println("Buzz");
-                default -> System.out.println(i);
-            }
+            printFizzBuzzForValueWithSwitch(i);
         }
 
+    }
+
+    private static String printFizzBuzzForValueWithSwitch(int i) {
+        return switch (i % 15) {
+            case 0 -> "FizzBuzz";
+            case 3, 6, 9, 12 -> "Fizz";
+            case 5, 10 -> "Buzz";
+            default -> "" + i; // Integer.toString(i);
+        }
+    }
+
+    private static void printFizzBuzzForValue(int i) {
+        if (i % 3 == 0 && i % 5 == 0)           // (i % 3*5 == 0) eller (i % 15 == 0) Bara delbart med 15
+            System.out.print("FizzBuzz, ");
+        else if (i % 3 == 0)
+            System.out.print("Fizz, ");
+        else if (i % 5 == 0)
+            System.out.print("Buzz, ");
+        else
+            System.out.print(i + ", ");
     }
 }
